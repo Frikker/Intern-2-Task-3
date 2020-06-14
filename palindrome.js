1893d3bc95593f326a019044fcb1d11895b109cb
Frikker <stolpnikk.97@gmail.com>
2020-06-14 15:24:44 +0300

    export default for palinfrome

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,4 @@
-function isPalindrome(string) {
+export default function isPalindrome(string) {
     string = string.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
     if (string.length == 1) {
         return true;
